6266e09fca1b5f6a3d8470d332239a0257533566
Akhil Mohan <akhilm@getrealhealth.com>
2017-07-14 14:19:17 +0530

    new class DBConnectExecute

---
+++ InventoryManagement/InventoryManagement/DBConnectExecute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement
+{
+    public class DBConnectExecute
+    {
+        public SqlConnection Connect()
+        {
+            SqlConnection Myconn;
+            string MyConnection2 = "Data Source=KL-DEV6;Initial Catalog=Inventory;User ID=sa";
+            Myconn = new SqlConnection(MyConnection2);
+            return Myconn;
+        }
+    }
+}
