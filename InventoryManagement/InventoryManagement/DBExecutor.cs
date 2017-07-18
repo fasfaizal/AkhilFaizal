736d5752a35238e1d08fa100672b6cfcf27612c5
Faizal P.A <faizal@getrealhealth.com>
2017-07-18 15:00:52 +0530

    Adding DBExecutor.cs Starting Generics

---
+++ InventoryManagement/InventoryManagement/DBExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace InventoryManagement
+{
+    public class DBExecutor<T>
+    {
+        public delegate T Exec(SqlConnection myConn);
+        public T Execute(Exec exeObj)
+        {
+            string MyConnection2 = WebConfigurationManager.AppSettings["ConnectionString"];
+            SqlConnection myConn=myConn = new SqlConnection(MyConnection2);
+            myConn.Open();
+            T ret=exeObj.Invoke(myConn);
+            myConn.Close();
+            return ret;
+        }
+    }
+}
